b0e90ac99763095692bcff517d33f9c9fde74248
umangsv <umangsv1987@gmail,com>
2020-02-22 17:27:12 +0530

    this is first commit

---
+++ src/src/Test1.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Test1 {
+    public static void main(String args[]) {
+        System.out.println("lets print on console");
+        System.out.println("lets print another line");
+    }
+}
---
+++ src/src/Test2.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Test2 {
+    public static void main(String args[]) {
+        System.out.println("lets print on console in Test2");
+        System.out.println("lets print another line in Test2");
+    }
+}
