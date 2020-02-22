5cf9a7e462be202d17563937a5be80197a157fc3
umangsv <umangsv1987@gmail,com>
2020-02-22 17:38:24 +0530

    commit for branch2

---
+++ src/src/Test4ForBranch2.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Test4ForBranch2 {
+    public static void main(String args[]) {
+        System.out.println("lets print on console");
+        System.out.println("lets print another line");
+        System.out.println("another line for branch2");
+    }
+}
