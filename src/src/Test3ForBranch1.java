fc49656c2dfc1e17621302f39878a4aa89ba2ae9
umangsv <umangsv1987@gmail,com>
2020-02-22 17:33:39 +0530

    checkin code to branch1

---
+++ src/src/Test3ForBranch1.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Test3ForBranch1 {
+    public static void main(String args[]) {
+        System.out.println("lets print on console in Test1 in Branch1");
+        System.out.println("lets print another line in Test1 in Branch1");
+        System.out.println("another line for branch1");
+    }
+}
